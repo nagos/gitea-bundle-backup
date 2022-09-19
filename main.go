c6109dcb0070462ca7ca4439e6bd6632c5efb15c
Vladimir Yakovlev <nagos@inbox.ru>
2022-09-19 14:16:14 +0300

    Создание bundle

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 var gitea Gitea
@@ -27,10 +31,43 @@ func parseArgs() bool {
 	return false
 }
 
+func gitBundle(repo string) {
+	fmt.Println(repo)
+	url := strings.Split(repo, "/")
+	repo_name := url[len(url)-1]
+	repo_user := url[len(url)-2]
+	tmp_dir, err := ioutil.TempDir("", "gitea_backup")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmp_dir)
+
+	cmd := exec.Command("git", "clone", "--mirror", repo, tmp_dir)
+	_, err = cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	cwd, _ := os.Getwd()
+	cmd = exec.Command("git", "bundle", "create", fmt.Sprintf("%s/%s_%s", cwd, repo_user, repo_name), "--all")
+	cmd.Dir = tmp_dir
+	_, err = cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
 func main() {
 	if parseArgs() {
 		os.Exit(1)
 	}
 
-	fmt.Println(gitea.Repos())
+	repos := gitea.Repos()
+	for _, i := range repos {
+		gitBundle(i)
+	}
 }
