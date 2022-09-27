ef83aeb91fd378a2b916e19fc02ae7bab85c72c6
Vladimir Yakovlev <nagos@inbox.ru>
2022-09-27 16:19:42 +0300

    Обработка ошибок

--- gitea.go
+++ gitea.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 )
+
+var ErrorApi = errors.New("API error")
 
 type org struct {
 	Username string `json:"username"`
@@ -24,15 +27,18 @@ type Gitea struct {
 	key string
 }
 
-func (g *Gitea) getOrgs() []string {
+func (g *Gitea) getOrgs() ([]string, error) {
 	var ret []string
 
 	resp, err := http.Get(fmt.Sprintf("%s/api/v1/orgs?access_token=%s", g.url, g.key))
 	if err != nil {
-		fmt.Println("API error", err)
-		return ret
+		return ret, ErrorApi
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return ret, ErrorApi
+	}
 
 	body, _ := io.ReadAll(resp.Body)
 
@@ -42,18 +48,21 @@ func (g *Gitea) getOrgs() []string {
 	for _, i := range res {
 		ret = append(ret, i.Username)
 	}
-	return ret
+	return ret, nil
 }
 
-func (g *Gitea) getUsers() []string {
+func (g *Gitea) getUsers() ([]string, error) {
 	var ret []string
 
 	resp, err := http.Get(fmt.Sprintf("%s/api/v1/admin/users?access_token=%s", g.url, g.key))
 	if err != nil {
-		fmt.Println("API error", err)
-		return ret
+		return ret, ErrorApi
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return ret, ErrorApi
+	}
 
 	body, _ := io.ReadAll(resp.Body)
 
@@ -63,18 +72,20 @@ func (g *Gitea) getUsers() []string {
 	for _, i := range res {
 		ret = append(ret, i.Login)
 	}
-	return ret
+	return ret, nil
 }
 
-func (g *Gitea) getOrgReposPage(org string, page int) []string {
+func (g *Gitea) getOrgReposPage(org string, page int) ([]string, error) {
 	var ret []string
 
 	resp, err := http.Get(fmt.Sprintf("%s/api/v1/orgs/%s/repos?page=%d&access_token=%s", g.url, org, page, g.key))
 	if err != nil {
-		fmt.Println("API error", err)
-		return ret
+		return ret, ErrorApi
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return ret, ErrorApi
+	}
 
 	body, _ := io.ReadAll(resp.Body)
 
@@ -85,32 +96,37 @@ func (g *Gitea) getOrgReposPage(org string, page int) []string {
 		ret = append(ret, i.CloneUrl)
 	}
 
-	return ret
+	return ret, nil
 }
 
-func (g *Gitea) getOrgRepos(org string) []string {
+func (g *Gitea) getOrgRepos(org string) ([]string, error) {
 	var ret []string
 
 	for i := 0; ; i++ {
-		r := g.getOrgReposPage(org, i)
+		r, e := g.getOrgReposPage(org, i)
+		if e != nil {
+			return ret, e
+		}
 		if len(r) == 0 {
 			break
 		}
 		ret = append(ret, r...)
 	}
 
-	return ret
+	return ret, nil
 }
 
-func (g *Gitea) getUserReposPage(user string, page int) []string {
+func (g *Gitea) getUserReposPage(user string, page int) ([]string, error) {
 	var ret []string
 
 	resp, err := http.Get(fmt.Sprintf("%s/api/v1/users/%s/repos?page=%d&access_token=%s", g.url, user, page, g.key))
 	if err != nil {
-		fmt.Println("API error", err)
-		return ret
+		return ret, ErrorApi
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		return ret, ErrorApi
+	}
 
 	body, _ := io.ReadAll(resp.Body)
 
@@ -121,35 +137,57 @@ func (g *Gitea) getUserReposPage(user string, page int) []string {
 		ret = append(ret, i.CloneUrl)
 	}
 
-	return ret
+	return ret, nil
 }
 
-func (g *Gitea) getUserRepos(user string) []string {
+func (g *Gitea) getUserRepos(user string) ([]string, error) {
 	var ret []string
 
 	for i := 0; ; i++ {
-		r := g.getUserReposPage(user, i)
+		r, e := g.getUserReposPage(user, i)
+		if e != nil {
+			return ret, e
+		}
 		if len(r) == 0 {
 			break
 		}
 		ret = append(ret, r...)
 	}
 
-	return ret
+	return ret, nil
 }
 
-func (g *Gitea) Repos() []string {
+func (g *Gitea) Repos() ([]string, error) {
 	var ret []string
 
-	for _, org := range g.getOrgs() {
-		ret = append(ret, g.getOrgRepos(org)...)
+	orgs, e := g.getOrgs()
+	if e != nil {
+		return ret, e
+	}
+	users, e := g.getUsers()
+	if e != nil {
+		return ret, e
 	}
 
-	for _, org := range g.getUsers() {
-		ret = append(ret, g.getUserRepos(org)...)
+	// org repos
+	for _, org := range orgs {
+		r, e := g.getOrgRepos(org)
+		if e != nil {
+			return ret, e
+		}
+		ret = append(ret, r...)
 	}
 
-	return ret
+	// user repos
+	for _, org := range users {
+		r, e := g.getUserRepos(org)
+		if e != nil {
+			return ret, e
+		}
+		ret = append(ret, r...)
+	}
+
+	return ret, nil
 }
 
 func (g *Gitea) SetUrl(url string) {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -17,44 +18,49 @@ func myUsage() {
 	flag.PrintDefaults()
 }
 
-func parseArgs() bool {
+func parseArgs() error {
 	flag.Usage = myUsage
 	flag.Parse()
 
 	if flag.NArg() != 2 {
 		flag.Usage()
-		return true
+		return errors.New("bad arguments")
 	}
 
 	gitea.SetUrl(flag.Arg(0))
 	gitea.SetApiKey(flag.Arg(1))
-	return false
+	return nil
+}
+
+func shellRun(cmd string, dir string, args ...string) error {
+	c := exec.Command(cmd, args...)
+	if len(dir) != 0 {
+		c.Dir = dir
+	}
+	_, err := c.Output()
+
+	return err
 }
 
 func gitBundle(repo string) {
-	fmt.Println(repo)
 	url := strings.Split(repo, "/")
 	repo_name := url[len(url)-1]
 	repo_user := url[len(url)-2]
+	fmt.Printf("Bundling %s/%s\n", repo_user, repo_name)
 	tmp_dir, err := ioutil.TempDir("", "gitea_backup")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer os.RemoveAll(tmp_dir)
 
-	cmd := exec.Command("git", "clone", "--mirror", repo, tmp_dir)
-	_, err = cmd.Output()
-
+	err = shellRun("git", "", "clone", "--mirror", repo, tmp_dir)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Println(err.Error())
 		return
 	}
 
 	cwd, _ := os.Getwd()
-	cmd = exec.Command("git", "bundle", "create", fmt.Sprintf("%s/%s_%s", cwd, repo_user, repo_name), "--all")
-	cmd.Dir = tmp_dir
-	_, err = cmd.Output()
-
+	err = shellRun("git", cwd, "bundle", "create", fmt.Sprintf("%s/%s_%s", cwd, repo_user, repo_name), "--all")
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -62,11 +68,14 @@ func gitBundle(repo string) {
 }
 
 func main() {
-	if parseArgs() {
-		os.Exit(1)
+	if e := parseArgs(); e != nil {
+		log.Fatal("Failed to run program: ", e.Error())
 	}
 
-	repos := gitea.Repos()
+	repos, e := gitea.Repos()
+	if e != nil {
+		log.Fatal("Cannot bundle repos: ", e.Error())
+	}
 	for _, i := range repos {
 		gitBundle(i)
 	}
